400eaaf3e73a89c221658b7cf58d106926c4f774
masiiie <61882387+masiiie@users.noreply.github.com>
2021-10-16 13:32:02 -0400

    apareando-patrones

---
+++ Weboo.ApareandoPatrones/Weboo.ApareandoPatrones.Tester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weboo.ApareandoPatrones.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Examen.CantidadCoincidencias("Hola Mundo", "Hola Mundo"));        // 1
+            //Console.WriteLine(Examen.CantidadCoincidencias("Hola Mundo", "Hola Mundo!"));       // 0
+            //Console.WriteLine(Examen.CantidadCoincidencias("?a?", "aa"));                       // 2
+            //Console.WriteLine(Examen.CantidadCoincidencias("!+*", "abaa"));                     // 3
+            //Console.WriteLine(Examen.CantidadCoincidencias("*?", ""));                          // 1
+            //Console.WriteLine(Examen.CantidadCoincidencias("*?", "Otra cadena cualquiera"));    // 2
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine(Examen.CantidadCoincidencias("*+?","juego"));
+            //Console.WriteLine(Examen.CantidadCoincidencias("?","juego"));
+            //Console.WriteLine(Examen.CantidadCoincidencias("*","juego"));
+            //Console.WriteLine(Examen.CantidadCoincidencias("*???+","juego"));
+            //Console.WriteLine(Examen.CantidadCoincidencias("+",""));
+
+            Examen.Coincidencias();
+        }
+    }
+}
---
+++ Weboo.ApareandoPatrones/Weboo.ApareandoPatrones/Examen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weboo.ApareandoPatrones
+{
+    public static class Examen
+    {
+        public static int CantidadCoincidencias(string patron, string cadena)
+        {
+            var diccionario = ConstruirDiccionario(cadena);
+            return Coincidencias(0, cadena, 0, patron, diccionario);
+        }
+        public static void Coincidencias()
+        {
+            Console.WriteLine("Cadena");
+            var cadena = Console.ReadLine();
+            Console.WriteLine("Patron");
+            var patron = Console.ReadLine();
+            var dicci = ConstruirDiccionario(cadena);
+            Console.WriteLine(Coincidencias(0, cadena, 0, patron, dicci));
+        }
+        static int Coincidencias(int pos, string cadena, int poscadena, string patron, Dictionary<char, Tuple<int, int>> comodines)
+        {
+            if (poscadena == cadena.Length && pos == patron.Length) return 1;
+            else
+            {
+                int sol = 0;
+                if (pos < patron.Length && comodines.Keys.Contains(patron[pos]))
+                {
+                    int min = comodines[patron[pos]].Item1;
+                    int max = comodines[patron[pos]].Item2;
+                    for (int j = min; j < max + 1; j++)
+                    {
+                        int posf = poscadena + j;
+                        if (posf <= cadena.Length && posf > -1) sol += Coincidencias(pos + 1, cadena, posf, patron, comodines);
+                    }
+                }
+                else
+                {
+                    if (pos < patron.Length && poscadena < cadena.Length && patron[pos] == cadena[poscadena]) sol += Coincidencias(pos + 1, cadena, poscadena + 1, patron, comodines);
+                }
+                return sol;
+            }
+        }
+        static Dictionary<char, Tuple<int, int>> ConstruirDiccionario(string cadena)
+        {
+            var comodines = new Dictionary<char, Tuple<int, int>>();
+            comodines.Add('!', new Tuple<int, int>(1, 1));
+            comodines.Add('?', new Tuple<int, int>(0, 1));
+            comodines.Add('*', new Tuple<int, int>(0, cadena.Length));
+            comodines.Add('+', new Tuple<int, int>(1, cadena.Length));
+            return comodines;
+        }
+    }
+}
