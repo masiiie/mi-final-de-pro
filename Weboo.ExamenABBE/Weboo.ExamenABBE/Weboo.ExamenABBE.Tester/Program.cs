ec0c96e70e1e89da15e12435488d64e2b783fef8
masiiie <61882387+masiiie@users.noreply.github.com>
2021-10-16 13:32:41 -0400

    arboles-examen

---
+++ Weboo.ExamenABBE/Weboo.ExamenABBE/Weboo.ExamenABBE.Tester/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Weboo.ExamenABBE.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABBE<int> t1 = new ABBE<int>();
+
+            t1.Inserta(4);
+            t1.Inserta(3);
+            t1.Inserta(8);
+            t1.Inserta(5);
+            t1.Inserta(2);
+
+            foreach (var f in t1.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // 2: 1
+            // 3: 2
+            // 4: 3
+            // 5: 1
+            // 8: 2
+
+
+
+            ABBE<string> t2 = new ABBE<string>();
+
+            t2.Inserta("c");
+            foreach (var f in t2.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // c: 1
+
+            t2.Inserta("a");
+            foreach (var f in t2.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // a: 1
+            // c: 2
+
+            t2.Inserta("i");
+            foreach (var f in t2.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // a: 1
+            // c: 3
+            // i: 1
+
+            t2.Inserta("o");
+            foreach (var f in t2.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // a: 1
+            // c: 3
+            // i: 2
+            // o: 1
+
+            t2.Inserta("n");
+            foreach (var f in t2.FrecuenciaPorValor())
+                Console.WriteLine("{0}: {1}", f.Valor, f.Cantidad);
+            Console.WriteLine();
+
+            // a: 1
+            // c: 3
+            // i: 2
+            // o: 2
+            // n: 1
+
+            var m = new ABBE<string>();
+            m.Inserta("a");
+            m.Inserta("z");
+            m.Inserta("K");
+            foreach (var item in m.FrecuenciaPorValor())
+            {
+                Console.WriteLine(item.Valor+": "+item.Cantidad);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Weboo.ExamenABBE/Weboo.ExamenABBE/Weboo.ExamenABBE/ABBE.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Weboo.ExamenABBE.Utils;
+
+namespace Weboo.ExamenABBE
+{
+    public class ABBE<T> where T : IComparable<T>
+    {
+        T val;
+        public T Valor { get { return val; } set { val = value; } }
+        ABBE<T> iz;
+        ABBE<T> der;
+        public ABBE<T> Izquierdo { get { return iz; } set { iz = value; } }
+        public ABBE<T> Derecho { get { return der; } set { der = value; } }
+        public T Maximo
+        {
+            get
+            {
+                if (this.Derecho == null) return this.Valor;
+                else return this.Derecho.Maximo;
+            }
+        }
+        public T Minimo
+        {
+            get
+            {
+                if (this.Izquierdo == null) return this.Valor;
+                else return this.Izquierdo.Minimo;
+            }
+        }
+        ABBE(T valor, ABBE<T> izquierdo, ABBE<T> derecho)
+        {
+            if (izquierdo.Valor.CompareTo(valor) != 0)
+            {
+                if (derecho.Valor.CompareTo(valor) != 0) throw new Exception("Parametro invalido");
+            }
+            if (izquierdo.Maximo.CompareTo(derecho.Minimo) == 1) throw new Exception("Parametro invalido");
+
+            val = valor;
+            iz = izquierdo;
+            der = derecho;
+        }
+        public ABBE()
+        {
+            val = default(T);
+            iz = null; der = null;
+        }
+        ABBE(T valor)
+        {
+            val = valor;
+            der = null; iz = null;
+        }
+
+        public void Inserta(T valor)
+        {
+            if (this.EsHoja() && this.Valor==null || this.Valor.CompareTo(default(T))==0) this.Valor = valor;
+            else if (this.EsHoja())
+            {
+                if (valor.CompareTo(this.Valor) == -1)
+                {
+                    this.Izquierdo = new ABBE<T>(valor);
+                    this.Derecho = new ABBE<T>(this.Valor);
+                }
+                else
+                {
+                    this.Derecho = new ABBE<T>(valor);
+                    this.Izquierdo = new ABBE<T>(this.Valor);
+                }
+            }
+            else
+            {
+                if (valor.CompareTo(this.Valor) == -1) this.Izquierdo.Inserta(valor);
+                else if (valor.CompareTo(this.Valor) == 1) this.Derecho.Inserta(valor);
+            }
+        }
+        public bool EsHoja()
+        {
+            if (this.Izquierdo == null && this.Derecho == null) return true;
+            return false;
+        }
+
+        public IEnumerable<Frecuencia<T>> FrecuenciaPorValor()
+        {
+            var result = new List<Frecuencia<T>>();
+            var aux = new List<Frecuence<T>>();
+            var annadidos = new List<T>();
+            foreach (T item in this.EntreOrden())
+            {
+                if (annadidos.Contains(item))
+                {
+                    foreach (Frecuence<T> element in aux)
+                    {
+                        if (element.valor.CompareTo(item) == 0)
+                        {
+                            element.Cantidad++;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    annadidos.Add(item);
+                    var fre = new Frecuence<T>();
+                    fre.valor = item;
+                    fre.Cantidad = 1;
+                    aux.Add(fre);
+                }
+            }
+
+            //ahora voy a pasar los valores a la lista original
+            foreach (Frecuence<T> item in aux)
+            {
+                result.Add(new Frecuencia<T>(item.valor, item.Cantidad));
+            }
+            return result;
+
+        }
+        public IEnumerable<T> EntreOrden()
+        {
+            if (this.EsHoja()) yield return this.Valor;
+            else
+            {
+                foreach (T item in this.Izquierdo.EntreOrden())
+                {
+                    yield return item;
+                }
+                yield return this.Valor;
+                foreach (T item in this.Derecho.EntreOrden())
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+    public class Frecuence<T> where T : IComparable<T>
+    {
+        public T valor;
+        public int Cantidad;
+        public Frecuence()
+        {
+            valor = default(T);
+            Cantidad = 0;
+        }
+    }
+}
